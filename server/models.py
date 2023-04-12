758b09f819332fd5e637867eab1433f520792aa3
betalantz <betalantz@gmail.com>
2023-04-12 12:04:09 -0700

    add validation to Appearance for rating

--- server/models.py
+++ server/models.py
@@ -56,5 +56,11 @@ class Appearance(db.Model, SerializerMixin):
     episode_id = db.Column(db.Integer, db.ForeignKey('episodes.id'))
     guest_id = db.Column(db.Integer, db.ForeignKey('guests.id'))
 
+    @validates('rating')
+    def validate_rating(self, key, rating):
+        if rating < 1 or rating > 5:
+            raise ValueError('Rating must be between 1 and 5')
+        return rating
+
     def __repr__(self):
         return f'<Appearance (id={self.id}, rating={self.rating}, episode={self.episode}, guest={self.guest})>'
