1ad5f8351a97d1b84377076e375e173183f12503
betalantz <betalantz@gmail.com>
2023-04-12 12:02:03 -0700

    add attributes to models

--- server/models.py
+++ server/models.py
@@ -17,15 +17,44 @@ class Episode(db.Model, SerializerMixin):
     __tablename__ = 'episodes'
 
     id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String)
+    number = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    appearances = db.relationship('Appearance', backref='episode')
+    guests = association_proxy('appearances', 'guest')
+
+    def __repr__(self):
+        return f'<Episode (id={self.id}, date={self.date}, number={self.number})>'
 
 class Guest(db.Model, SerializerMixin):
     __tablename__ = 'guests'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    occupation = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    appearances = db.relationship('Appearance', backref='guest')
+    episodes = association_proxy('appearances', 'episode')
+
+    def __repr__(self):
+        return f'<Guest (id={self.id}, name={self.name}, occupation={self.occupation})>'
+
 
 class Appearance(db.Model, SerializerMixin):
     __tablename__ = 'appearances'
+    serialize_rules = ('-episode.appearances', '-guest.appearances')
 
     id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-# add any models you may need. 
+    episode_id = db.Column(db.Integer, db.ForeignKey('episodes.id'))
+    guest_id = db.Column(db.Integer, db.ForeignKey('guests.id'))
+
+    def __repr__(self):
+        return f'<Appearance (id={self.id}, rating={self.rating}, episode={self.episode}, guest={self.guest})>'
